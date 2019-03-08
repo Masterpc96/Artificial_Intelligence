34a33124947e43cdca231fe8ae76c242f58f30d6
Michael Urban <michaelurban@Michaels-MacBook-Pro-2.local>
2019-03-08 12:06:19 +0100

    generate routes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String args[]){
+
+
         Trial trial = new Trial("data/easy_1.ttp");
-        System.out.println(trial.toString());
+        ArrayList<Route> routes = new ArrayList<>(100);
+        for (int i=0; i <100; i++){
+            routes.add(new Route(i,trial.getPlaces()));
+            System.out.println(routes.get(i).toString());
+        }
+
     }
 }
