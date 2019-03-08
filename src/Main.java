f2636700d5f83b0fb0b2a89507eadf1b00baeb4d
Michael Urban <michaelurban@Michaels-MacBook-Pro-2.local>
2019-03-08 16:35:17 +0100

    init commit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,12 @@ public class Main {
         Trial trial = new Trial("data/easy_0.ttp");
         ArrayList<Route> routes = new ArrayList<>(100);
         for (int i=0; i <100; i++){
+            // rand population
             routes.add(new Route(i,trial.getPlaces(), trial.getMaxSpeed(), trial.getMinSpeed(), trial.getKnapsackCapacity()));
+
+            // collect items
+            routes.get(i).evaluate();
+
             System.out.println(routes.get(i).toString());
             System.out.println();
         }
