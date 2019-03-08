cefee0f0dca3f9b98544afe0559eb56d956e779a
Michael Urban <michaelurban@Michaels-MacBook-Pro-2.local>
2019-03-08 13:57:17 +0100

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,12 @@ public class Main {
     public static void main(String args[]){
 
 
-        Trial trial = new Trial("data/easy_1.ttp");
+        Trial trial = new Trial("data/easy_0.ttp");
         ArrayList<Route> routes = new ArrayList<>(100);
         for (int i=0; i <100; i++){
-            routes.add(new Route(i,trial.getPlaces()));
+            routes.add(new Route(i,trial.getPlaces(), trial.getMaxSpeed(), trial.getMinSpeed(), trial.getKnapsackCapacity()));
             System.out.println(routes.get(i).toString());
+            System.out.println();
         }
 
     }
